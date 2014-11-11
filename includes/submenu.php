4474c7182389fc0cc981927a745a2995cbcf7cc7
Alex <220619@gmail.com>
2014-11-11 15:51:50 +0300

    submenu children

--- includes/submenu.php
+++ includes/submenu.php
@@ -1,14 +1,26 @@
 <?php class_exists('Core', false) or die();
 
 $query = Inner::set();
-$query->condition = 'menu = 1 OR parent = '.Core::$item['id'];
+$query->condition = 'parent = '.Core::$item['id'];
 $query->order = 'ord ASC';
 
 if($rows = Inner::get($query)){
     echo ' <ul class="submenu section">';
         foreach($rows as $row) {
-            echo '<li><a href="/'.$row['alias'].'/">'.$row['name'].'</a></li>';
+            echo '<li><a href="/'.Core::$item['alias'].'/'.$row['alias'].'/">'.$row['name'].'</a></li>';
         }
     echo '</ul> ';
+}else {
+    $query = Inner::set();
+    $query->condition = 'menu = 1';
+    $query->order = 'ord ASC';
+
+    if ($rows = Inner::get($query)) {
+        echo ' <ul class="submenu section">';
+        foreach ($rows as $row) {
+            echo '<li><a href="/' . $row['alias'] . '/">' . $row['name'] . '</a></li>';
+        }
+        echo '</ul> ';
+    }
 }
 ?>
