b87c47684b37fa1ec53a48f1a6c4b54fdb6c66e7
Alex <220619@gmail.com>
2014-11-12 11:13:08 +0300

    todo 121114

--- includes/menu1.php
+++ includes/menu1.php
@@ -5,9 +5,7 @@ $query->condition = 'tour > 0';
 $query->order = 'ord ASC';
 $set = Inner::get($query);
 
-$i =0;
 foreach($set as $row) {
-    echo '<a href="/'.$row['alias'].'/" class="'.(Router::$request->parsed->origin[0] == $row['alias'] ? 'active ' : '').'f'.$i.'"><i></i><span>'.$row['name'].'</span></a>';
-    $i++;
+    echo '<a href="/'.$row['alias'].'/" class="'.(Router::$request->parsed->origin[0] == $row['alias'] ? 'active ' : '').'f'.$row['id'].'"><i></i><span>'.$row['name'].'</span></a>';
 }
 ?>
