3494042ed0643de14acbfc80eedf6a69262c5626
Alex <220619@gmail.com>
2014-12-04 17:39:14 +0300

    prod

--- includes/submenu.php
+++ includes/submenu.php
@@ -5,24 +5,32 @@ $query->condition = 'parent = '.Core::$item['id'].(Core::$item['parent'] ? ' OR 
 $query->order = 'ord ASC';
 
 if($rows = Inner::get($query)){
-    echo ' <ul class="submenu section">';
-        foreach($rows as $row) {
-            $active = Core::$item['alias'] == $row['alias'] ? ' class="active"' : '';
-            echo '<li'.$active.'><a href="/'.Router::$request->parsed->origin[0].'/'.$row['alias'].'/">'.$row['name'].'</a></li>';
-        }
-    echo '</ul> ';
+    echo '
+    <div class="submenu">
+        <div class="fold"></div>
+        <ul class="submenu section">';
+            foreach($rows as $row) {
+                $active = Core::$item['alias'] == $row['alias'] ? ' class="active"' : '';
+                echo '<li'.$active.'><a href="/'.Router::$request->parsed->origin[0].'/'.$row['alias'].'/">'.$row['name'].'</a></li>';
+            }
+        echo '</ul>
+    </div>';
 }else {
     $query = Inner::set();
     $query->condition = 'menu = 1';
     $query->order = 'ord ASC';
 
     if ($rows = Inner::get($query)) {
-        echo ' <ul class="submenu section">';
-        foreach ($rows as $row) {
-            $active = Core::$item['alias'] == $row['alias'] ? ' class="active"' : '';
-            echo '<li'.$active.'><a href="/' . $row['alias'] . '/">' . $row['name'] . '</a></li>';
-        }
-        echo '</ul> ';
+        echo '
+        <div class="submenu">
+            <div class="fold"></div>
+            <ul class="section">';
+            foreach ($rows as $row) {
+                $active = Core::$item['alias'] == $row['alias'] ? ' class="active"' : '';
+                echo '<li'.$active.'><a href="/' . $row['alias'] . '/">' . $row['name'] . '</a></li>';
+            }
+            echo '</ul>
+        </div>';
     }
 }
 ?>
